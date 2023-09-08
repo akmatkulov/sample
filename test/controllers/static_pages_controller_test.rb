80ba8655192e8de19ef3cbea97c2ceed6ce65414
akmatkulov <akmatkulovs991@gmail.com>
2023-09-09 00:25:49 +0600

    Complete chappter4

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,25 +4,23 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
-  #должен попасть на страницу home
+
   test "should get home" do
-    #запрос GET методу home
-    get static_pages_home_url
-    #должен вернутся код состояние 200ок или success
+    get root_path
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   #должен попасть на страницу help
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
 
   #должен попасть на страницу about
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
