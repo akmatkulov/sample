9436eeca51a1d69baf061a4bc97173d72cea6c06
akmatkulov <akmatkulovs991@gmail.com>
2023-09-12 16:21:33 +0600

    Add action edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "User updated"
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
   def create
     @user = User.new(user_params)
     if @user.save
